a6c69a58f4b2f62434dcf83865c7742cac3438be
augustoaccorsi <augusto.acorsi@gmail.com>
2021-09-22 09:55:04 -0300

    add fizzbuzz command line application

---
+++ 2. Spring Boot 2.0 Essential Training/booting-web-et/fizzbuzz/src/test/java/com/augustoaccorsi/lil/sbet/fizzbuzz/fizzbuzz/FizzbuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustoaccorsi.lil.sbet.fizzbuzz.fizzbuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FizzbuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
